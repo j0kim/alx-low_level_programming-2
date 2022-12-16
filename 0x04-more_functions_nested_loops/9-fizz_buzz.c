13dec9010d0068c8b9415a031be4f75946f9f6d1
neryomollo <neryomollo@gmail.com>
2022-12-16 09:14:33 -0800

    fixed error

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,10 +3,11 @@
 
 /**
  * main - prints a Fizz Buzz test program
- *
  * Return: Always 0 is success
  */
+
 int main(void)
+
 {
 	int x;
 
@@ -20,7 +21,7 @@ int main(void)
 	{
 	printf("Fizz");
 	}
-	else if (x % 5 ==0)
+	else if (x % 5 == 0)
 	{
 	printf("Buzz");
 	}
